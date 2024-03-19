8d97e02eaa84f862c1470dbca2073a3377f962a3
Kim <lumierebuds@gmail.com>
2024-03-19 22:27:07 +0900

    feat:change_policy

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -20,18 +21,20 @@ public class AppConfig {
     }
 
 
-
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
 
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
-        return discountPolicy();
+    public DiscountPolicy discountPolicy() {
+         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
+
 
 
 }
